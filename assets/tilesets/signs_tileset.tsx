9fae6408012a5e426ba481d2df37ec43bc4f3cbc
OllieSHunt <100690050+OllieSHunt@users.noreply.github.com>
2025-04-21 20:02:33 +0100

    Added exit sign and fixed spelling mistakes

---
+++ assets/tilesets/signs_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="signs_tileset" tilewidth="4" tileheight="4" tilecount="256" columns="16">
+ <image source="signs_tileset.png" width="64" height="64"/>
+</tileset>
